1c09c1c8ee8168b1ddf814e8f04d5073903f5d29
villetan <ville.tanskanen@helsinki.fi>
2016-01-19 19:35:59 +0200

    vk1 ennen herokua

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160117192247) do
+ActiveRecord::Schema.define(version: 20160119132801) do
+
+  create_table "beers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "style"
+    t.integer  "brewery_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "breweries", force: :cascade do |t|
     t.string   "name"
